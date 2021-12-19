0a4703fcef97b86820059fc517206b1484c9e86c
Zhenya Aleksandrova <laberfo@gmail.com>
2021-12-20 00:03:36 +0300

    added: script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const commonVariables = (eventTarget, data) => {
+	const employee = eventTarget.closest(".wrapper__tableTr").id;
+	const employeeId = +employee.id;
+	const chosenEmployee = data.employees.filter(
+		(employee) => employee.id === employeeId
+	);
+	const chooseEmployeeIndex = data.employees.findIndex(
+		(employee) => employee.id === chosenEmployee.id
+	);
+
+	return { employee, chosenEmployee, chooseEmployeeIndex };
+};
+
 const drawStuffList = (data) => {
 	const allEmployees = document.querySelector("#allEmployees");
 
@@ -10,10 +23,9 @@ const drawStuffList = (data) => {
 						<input
 							type="checkbox"
 							name="chooseEmployee"
-							id="checkbox"
 							class="wrapper__checkbox"
 						/>
-						<label for="checkbox"></label>
+
 					</div>
 				</th>
 				<th class="wrapper__tableTh tbodyTh">${employee.name}</th>
@@ -45,20 +57,42 @@ const addEmployees = (data) => {
 	drawStuffList(data);
 };
 
+const deleteEmployeeFromStuffList = (eventTarget, data) => {
+	const { employee, chosenEmployee, chooseEmployeeIndex } = commonVariables(
+		eventTarget,
+		data
+	);
+	const checkbox = document.getElementsByName("chooseEmployee");
+
+	for (let i = 0; checkbox[i]; i++) {
+		if (checkbox[i].checked) {
+			data.employees.splice(chooseEmployeeIndex, 1);
+		}
+	}
+	drawStuffList(data);
+	console.log(data);
+};//need to fix ! delete another <tr>. Doesn't see, where is checkbox
+
 const init = () => {
 	const stuff = document.querySelector("#stuff");
 	const addEmployeeBtn = document.querySelector("#addEmployeeBtn");
-	const checkbox = document.querySelector("#checkbox");
 
 	const data = {
 		employees: [],
-		chosen: [],
 	};
 
 	addEmployeeBtn.addEventListener("click", (event) => {
 		event.preventDefault();
 		addEmployees(data);
 		console.log(data);
+	});
+
+	stuff.addEventListener("click", (event) => {
+		switch (true) {
+			case [...event.target.classList].includes("btnFireAnEmployee"):
+				deleteEmployeeFromStuffList(event.target, data);
+				break;
+		}
 	});
 };
 init();
