0d793a2717c4b204b5fe48088ddac0a7656030ce
Zhenya Aleksandrova <laberfo@gmail.com>
2021-12-27 22:44:10 +0300

    added: all files

--- js/script.js
+++ js/script.js
@@ -1,31 +1,18 @@
-const commonVariables = (eventTarget, data) => {
-	const employee = eventTarget.closest(".wrapper__tableTr").id;
-	const employeeId = +employee.id;
-	const chosenEmployee = data.employees.filter(
-		(employee) => employee.id === employeeId
-	);
-	const chooseEmployeeIndex = data.employees.findIndex(
-		(employee) => employee.id === chosenEmployee.id
-	);
-
-	return { employee, chosenEmployee, chooseEmployeeIndex };
-};
-
 const drawStuffList = (data) => {
 	const allEmployees = document.querySelector("#allEmployees");
 
 	allEmployees.innerHTML = "";
 	data.employees.forEach((employee) => {
 		allEmployees.innerHTML += `
-			<tr class="wrapper__tableTr" id="${employee.id}">
+			<tr class="wrapper__tableTr">
 				<th class="wrapper__tableTh tbodyTh">
 					<div class="wrapper__checkboxWrap">
 						<input
 							type="checkbox"
 							name="chooseEmployee"
 							class="wrapper__checkbox"
+							id=${employee.id}
 						/>
-
 					</div>
 				</th>
 				<th class="wrapper__tableTh tbodyTh">${employee.name}</th>
@@ -43,7 +30,6 @@ const addEmployees = (data) => {
 	const inputDateOfBirth = document.querySelector("#inputDateOfBirth");
 	const inputDateOfEmployment = document.querySelector("#inputDateOfEmployment");
 	const inputWage = document.querySelector("#inputWage");
-	const checkbox = document.querySelector("#checkbox");
 
 	data.employees.push({
 		name: inputName.value,
@@ -51,31 +37,45 @@ const addEmployees = (data) => {
 		dateOfEmployment: inputDateOfEmployment.value,
 		wage: inputWage.value,
 		id: Date.now(),
+		checked: false,
 	});
 
 	addEmployeeForm.reset();
 	drawStuffList(data);
 };
 
-const deleteEmployeeFromStuffList = (eventTarget, data) => {
-	const { employee, chosenEmployee, chooseEmployeeIndex } = commonVariables(
-		eventTarget,
-		data
-	);
-	const checkbox = document.getElementsByName("chooseEmployee");
+const showNumberOfEmployes = (data) => {
+	const numberOfEmployees = document.querySelector("#numberOfEmployees");
+	numberOfEmployees.innerHTML = data.employees.length;
+};
 
-	for (let i = 0; checkbox[i]; i++) {
-		if (checkbox[i].checked) {
-			data.employees.splice(chooseEmployeeIndex, 1);
+const showAmountOfWages = (data) => {
+	const amountOfWages = document.querySelector("#amountOfWages");
+
+	let amount = 0;
+	data.employees.forEach((employee) => {
+		if (employee.checked === true) {
+			amount += +employee.wage;
 		}
-	}
+	});
+	amountOfWages.innerHTML = amount + " $";
+};
+
+const deleteEmployeeFromStuffList = (data) => {
+	data.employees.forEach((employee, index) => {
+		if (employee.checked === true) {
+			data.employees.splice(index, 1);
+		}
+	});
 	drawStuffList(data);
-	console.log(data);
-};//need to fix ! delete another <tr>. Doesn't see, where is checkbox
+	showNumberOfEmployes(data);
+}; //удаляет по одному
 
 const init = () => {
 	const stuff = document.querySelector("#stuff");
+	const allEmployees = document.querySelector("#allEmployees");
 	const addEmployeeBtn = document.querySelector("#addEmployeeBtn");
+	const deleteBtn = document.querySelector("#deleteBtn");
 
 	const data = {
 		employees: [],
@@ -84,13 +84,35 @@ const init = () => {
 	addEmployeeBtn.addEventListener("click", (event) => {
 		event.preventDefault();
 		addEmployees(data);
+		showNumberOfEmployes(data);
 		console.log(data);
+	});
+
+	allEmployees.addEventListener("click", (event) => {
+		if (event.target.tagName === "INPUT") {
+			const checkboxId = event.target.id;
+
+			data.employees.forEach((employee) => {
+				if (employee.id === +checkboxId) {
+					employee.checked = !employee.checked;
+				}
+				return employee;
+			});
+
+			console.log(data);
+		}
 	});
 
 	stuff.addEventListener("click", (event) => {
 		switch (true) {
 			case [...event.target.classList].includes("btnFireAnEmployee"):
-				deleteEmployeeFromStuffList(event.target, data);
+				deleteEmployeeFromStuffList(data);
+				showAmountOfWages(data);
+				console.log(data);
+				break;
+			case [...event.target.classList].includes("btnShowAmountOfWages"):
+				showAmountOfWages(data);
+				console.log(data);
 				break;
 		}
 	});
